7a24b1428f5635d9ab5487223b77f1c26c41242c
joseruiz010 <joruiz765@gmail.com>
2023-10-21 22:49:47 -0300

    agregue la validacion de campos

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,22 +1,23 @@
 const { check_email_password } = require("./usersController")
 const { decodeToken, generarToken } = require("../utils/JWT_Token")
+const { validarCampos } = require("../utils/validation.campos.ep")
 
 const login = async (req, res) => {
-  const { username, password } = req.body
-  if (!username || !password) res.send("Error")
+  const requiredField = ["username", "password"]
+  const { username, password, ...rest } = req.body
+  // if (!username || !password) res.send("Error")
+
+  const resValidacion = validarCampos(requiredField, req.body, res);
+
+  if (resValidacion) res.send({ message: resValidacion, data: null })
 
   try {
     const user = await check_email_password({ username, password })
-
-    if (!user.username) {
-      return res.send(401)
-    }
-
     const token =
       generarToken({
         id: user._id
       })
-    return res.send({ token })
+    return res.send({ message: resValidacion, data: { token } })
 
   } catch (error) {
     return res.send(401)
---
+++ src/utils/validation.campos.ep.js
@@ -0,0 +1,27 @@
+const validarCampos = (required_fields, body, res) => {
+
+  if (!existence_required_fields(required_fields, body))
+    return `Error data: los campos '${required_fields.join(', ')}' son requeridos`
+
+  if (Object.keys(body).length > required_fields.length) {
+    const camposTotales = Object.keys(body);
+    const camposAdicionales = camposTotales.filter(c => !required_fields.includes(c))
+    return `Información no válida. Los campos '${camposAdicionales.join(', ')}' no son requeridos.`
+  }
+  return null
+}
+const existence_required_fields = (required_fields, body) => {
+
+  let isValid = true;
+
+  if (required_fields.length > 0) {
+    required_fields.forEach(c => {
+      if (!body[c]) return isValid = false
+    })
+  }
+  return isValid;
+}
+
+module.exports = {
+  validarCampos
+}
